15c0d3aa05f0c12c3e7258a81bd791f2e8519f35
bcpractice <bcpractice@protonmail.com>
2022-03-17 19:03:54 -0600

    Add hover effect, edit button ui, and styles

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const gridContainer = document.querySelector(".grid-container");
+const grid = document.querySelector(".grid-container");
+const edit = document.querySelector(".edit-button");
 
-for (let i = 0; i < 16 * 16; i++) gridContainer.append( document.createElement("div") );
+function divideGrid() {
+    let input = parseInt( document.querySelector("#input").value );
+    while (grid.firstChild) grid.removeChild(grid.firstChild);
+    grid.style.gridTemplateRows = `repeat(${input}, auto)`;
+    grid.style.gridTemplateColumns = `repeat(${input}, auto)`;
+    for (let i = 0; i < input * input; i++) grid.append( document.createElement("div") );
+}
+
+function addHoverListener() {
+    grid.childNodes.forEach((child) => {
+        child.addEventListener("mouseover", () => animateDiv(child));
+    });
+}
+
+function animateDiv(child) {
+    child.style.backgroundColor = "#000";
+    let increments = 10;
+    for (let i = 0; i < increments; i++) {
+        let color = 255 * i / increments;
+        setTimeout(() => child.style.backgroundColor = `rgb(${color}, ${color}, ${color})`, i * 100);
+    }
+    setTimeout(child.style.backgroundColor = "", increments * 100);
+}
+
+for (let i = 0; i < 16 * 16; i++) grid.append( document.createElement("div") );
+addHoverListener();
+
+edit.addEventListener("click", () => {
+    divideGrid();
+    addHoverListener();
+});
